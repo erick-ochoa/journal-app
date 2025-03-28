88d2c2184e85a1767128727d53696ec021eb8aab
Erick Ochoa <erick.ochoa.vls@gmail.com>
2025-03-28 13:09:29 -0700

    Prueba de git desde terminal

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -6,9 +6,9 @@ import { RegisterPage } from "../pages/RegisterPage"
 export const AuthRoutes = () => {
   return (
     <Routes>
-        <Route path="login" element={ <LoginPage /> } />
-        <Route path="register" element={ <RegisterPage /> } />
-        <Route path="/*" element={ <Navigate to={ '/auth/login' } /> } />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="register" element={<RegisterPage />} />
+      <Route path="/*" element={<Navigate to={'/auth/login'} />} />
     </Routes>
   )
 }
