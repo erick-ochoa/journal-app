b27659c43d7eff302fd07a6514107686f6678d70
Erick Ochoa <erick.ochoa.vls@gmail.com>
2025-03-29 03:28:59 -0700

    Seccion 19 - clase 13

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,12 +1,38 @@
 import { Button, Grid, TextField, Link, Typography } from "@mui/material"
 import { AuthLayout } from "../layout/AuthLayout"
 import { Link as RouterLink } from "react-router-dom"
+import { useForm } from "../../hooks/useForm"
+import { useState } from "react"
+
+const formData = {
+  name: '',
+  email: '',
+  password: ''
+}
+
+const formValidtions = {
+  name: [(value) => value.length >= 1, 'EL nombre es obligatorio'],
+  email: [(value) => value.includes('@'), 'El correo debe de tener una @'],
+  password: [(value) => value.length >= 6, 'El password debe de tener minimo 6 caracteres']
+}
 
 export const RegisterPage = () => {
+
+  const [formSubmit, setFormSubmit] = useState(false)
+
+  const {
+    name, email, password, onInputChange, isFormValid, nameValid, emailValid, passwordValid
+  } = useForm(formData, formValidtions)
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setFormSubmit(true)
+  }
+
   return (
     <AuthLayout title="Crear cuenta">
 
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
 
           <Grid item xs={12} sx={{ mt: 2 }}>
@@ -15,6 +41,11 @@ export const RegisterPage = () => {
               placeholder="Erick Ochoa"
               type="text"
               fullWidth
+              name="name"
+              value={name}
+              onChange={onInputChange}
+              error={!!nameValid && formSubmit}
+              helperText={!!nameValid && formSubmit ? nameValid : null}
             />
           </Grid>
 
@@ -24,6 +55,11 @@ export const RegisterPage = () => {
               placeholder="correo@correo.com"
               type="email"
               fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
+              error={!!emailValid && formSubmit}
+              helperText={!!emailValid && formSubmit ? emailValid : null}
             />
           </Grid>
 
@@ -33,12 +69,17 @@ export const RegisterPage = () => {
               placeholder="password"
               type="password"
               fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
+              error={!!passwordValid && formSubmit}
+              helperText={!!passwordValid && formSubmit ? passwordValid : null}
             />
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: 1, mt: 1 }}>
             <Grid item xs={12}>
-              <Button variant="contained" fullWidth>Crear cuenta</Button>
+              <Button type="submit" variant="contained" fullWidth>Crear cuenta</Button>
             </Grid>
           </Grid>
 
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,24 +1,48 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
-export const useForm = ( initialForm = {} ) => {
-  
-    const [ formState, setFormState ] = useState( initialForm );
+export const useForm = (initialForm = {}, formValidations = {}) => {
+
+    const [formState, setFormState] = useState(initialForm);
+    const [formValidation, setFormValidation] = useState([]);
+
+    useEffect(() => {
+        createValdations();
+    }, [formState])
+
+    const isFormValid = useMemo(() => {
+        for (const formValue of Object.keys(formValidation)) {
+            if (formValidation[formValue] != null) return false
+        }
+        return true
+    }, [formValidation])
+
 
     const onInputChange = ({ target }) => {
         const { name, value } = target;
         setFormState({
             ...formState,
-            [ name ]: value
+            [name]: value
         });
     }
 
     const onResetForm = () => {
-        setFormState( initialForm );
+        setFormState(initialForm);
+    }
+
+    const createValdations = () => {
+        const formCheckValues = {}
+        for (const formFiel of Object.keys(formValidations)) {
+            const [fn, errorMessage] = formValidations[formFiel]
+            formCheckValues[`${formFiel}Valid`] = fn(formState[formFiel]) ? null : errorMessage
+        }
+        setFormValidation(formCheckValues)
     }
 
     return {
         ...formState,
+        ...formValidation,
         formState,
+        isFormValid,
         onInputChange,
         onResetForm,
     }
